6707da0d1696a6e454525ab5537ba3187c827019
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-29 20:29:57 -0600

    commit on master

---
+++ data-types.js
@@ -0,0 +1,27 @@
+// Boolean
+var truth = true;
+var notTruth = false;
+
+console.log(truth);
+
+// Null
+var nully = null;
+console.log(null);
+
+// Undefined
+var notDefined;
+console.log(notDefined);
+
+// Number
+var age = 12;
+console.log(age);
+
+// String
+var name = "Bryce";
+var nameTwo = 'Devyn';
+console.log(name);
+console.log(nameTwo);
+
+// Symbol
+var mySym = Symbol('foo bar');
+console.log(mySym);
