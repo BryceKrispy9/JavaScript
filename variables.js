cd13de7972649cdde9f4aa1ec13b1e093685987e
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-29 19:53:55 -0600

    commit on master

---
+++ variables.js
@@ -0,0 +1,14 @@
+// var age = 12;
+// let name = 'Justine';
+
+var name, city, age;
+
+name = 'Bryce';
+city = 'Salt Lake';
+age = 25
+
+console.log(age);
+console.log(name);
+console.log(city);
+
+// alert(name); // If we want a pop-up
