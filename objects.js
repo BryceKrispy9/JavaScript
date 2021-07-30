e08c2ae2e71a3b36d0f5d33bdc26deec560b7a97
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-29 21:07:56 -0600

    commit on master

---
+++ objects.js
@@ -0,0 +1,20 @@
+var user = { 
+    name: 'Bryce',
+    age: 25,
+    city: 'Springville',
+    grades: { // Nested object ***
+        math: 100,
+        english: 92,
+        science: 87
+    }
+}
+
+user.grades.coding = 99; // Adds a new key value pair inside of grades (WE CANNOT GO BACK AND CHANGE ORIGINAL CODE)
+
+console.log(user.grades); // User object
+console.log(user.grades.math); // Inside nested object
+
+// user.name = 'Devyn'; // Resets to a new name
+
+// console.log(user)
+// console.log(user.age); // Call the reference point in the object
