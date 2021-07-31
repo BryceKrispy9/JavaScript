46a92344c0d97500e64aac8d7d07e2402bcb24e3
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-30 18:38:28 -0600

    commit on master

---
+++ 7-30-21/arithmetic-operators.js
@@ -0,0 +1,29 @@
+2 + 2; or (2 + 2); // Addition
+
+2 - 3; // Subtraction
+
+10 / 2; // Division
+
+4 * 4; // Multiplication
+
+2 ** 10; // Exponents (2 to the power of 10)
+
+5 % 2; // Remainders (1) - Can also help you find if it's even or odd
+
+
+var num = 2;
+
+num++; // Returns 2 - Post fix notation (Placed at the end)
+num; // Returns 3
+++num; // Returns 4 - Pre fix notation
+
+num--; // Returns 4 - Decrementer
+--num; // Returns 2 - Pre Decrementer
+
+
+var someNum = 10;
+var someOtherNum = -someNum; // Returns negative
+
+
+var strNum = '100';
+var convertedNum = + strNum; // or + strNum - Changes it from a string to a num
