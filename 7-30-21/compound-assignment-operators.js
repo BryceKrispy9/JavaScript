0bf124197129b86d923d5211f416265f02cebdf0
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-30 18:45:22 -0600

    commit on master

---
+++ 7-30-21/compound-assignment-operators.js
@@ -0,0 +1,11 @@
+var name = 'Tiffany'; // '=' is assignment
+
+var sum = 0;
+var gradeOne = 100;
+var gradeTwo = 80;
+
+sum += gradeOne; // = 100
+
+sum += gradeTwo; // = 180 (sum = sum + grade***)
+
+sum *= gradeTwo // = 14400 (Multiplies)
