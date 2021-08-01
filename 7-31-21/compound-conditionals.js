2ddf3b73a94a3aa1bb9656c447d662f04f051fc0
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-31 21:22:12 -0600

    commit on master

--- 7-31-21/compound-conditionals.js
+++ 7-31-21/compound-conditionals.js
@@ -1,16 +1,15 @@
-var age = 15;
+var age = 30;
 
 if (age <= 10) {
-    console.log("You can eat from the kid's menu");
-    console.log("You are not old enough to drive");
-    console.log("You are not old enough to rent a car");
-} else if (age > 16 && age < 25) { // 'else if' is more specific
-    console.log("You cannot eat from the kid's menu");
-    console.log("You are old enough to drive");
-    console.log("You are not old enough to rent a car");
+  console.log("You can eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 16 && age < 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are not old enough to rent a car");
 } else if (age >= 25) {
-    console.log("You cannot eat from the kid's menu");
-    console.log("You are old enough to drive");
-    console.log("You are old enough to rent a car");
-} else (age == String);
-    console.log("Please enter a number")
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are old enough to rent a car");
+}
---
+++ 7-31-21/switch-statement.js
@@ -0,0 +1,26 @@
+var dataPoint = 'number';
+    
+    switch (typeof dataPoint) {
+    case "string":
+        console.log("5");
+        break;
+}
+
+
+
+
+
+
+// switch (typeof dataPoint) {
+//     case "string":
+//         console.log('It is a string!');
+//         break;
+//     case "number":
+//         console.log('It is a number!');
+//         break;
+//     case "boolean":
+//         console.log('It is either true or false (Boolean)');
+//         break;
+//     default:
+//         console.log('No matches');
+// }
