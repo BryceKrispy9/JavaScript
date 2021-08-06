ce23d916286db3bd6fa5bad986d9a07534267660
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-05 18:39:51 -0600

    commit on master

---
+++ 8-5-21/arrays.js
@@ -0,0 +1,15 @@
+var generatedArray = new Array(3);
+
+var generatedArray = new Array('Altuve', 'Correa', 'Spring');
+
+var myArray = [1, 2, 3];
+
+var myArray = ['Altuve', 'Correa', 'Spring'];
+
+myArray[0]; // How to pick out a value from an Array // myArray[0][1] (If nested)
+
+var playerName = myArray[1]; // Find what's at that index value
+
+var mixedArray = ['Hi', 1, ['a', 'b', 'c'], { name: 'Kristine' }, function greeting() { console.log('Hey there');}] // Collection of data = Array - Used for database queries
+
+mixedArray[4](); // Run function inside array
