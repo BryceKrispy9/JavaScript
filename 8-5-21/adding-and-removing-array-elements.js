23cfc7c053e767de8d841b32d6cbc7c43cf010ba
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-08 11:42:54 -0600

    commit on master

---
+++ 8-5-21/adding-and-removing-array-elements.js
@@ -0,0 +1,13 @@
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+arr.length; // 4
+
+arr.pop(); // Last item (Pops off from original array)
+
+arr.push('Bagwell'); // Adds items to end of array
+
+var elementPopped = arr.pop(); // Bagwell
+
+arr.shift(); // Removes Altuve
+
+arr.unshift('Kyle'); // Adds item to beginning of array
