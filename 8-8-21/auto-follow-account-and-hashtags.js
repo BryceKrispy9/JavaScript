2df8511b792b274a42602e8fa362231b25a0669c
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-08 20:04:21 -0600

    commit on master

---
+++ 8-8-21/auto-follow-account-and-hashtags.js
@@ -0,0 +1,18 @@
+// To follow on LinkedIn
+let hashtagBtns = document.querySelectorAll('.mn-discovery-hashtag-card__action-btn')
+
+hashtagBtns
+
+hashtagBtns.length
+
+hashtagBtns.forEach(btn => btn.click()) 
+
+// To unfollow
+const followingBtns = document.querySelectorAll('.follows-recommendation-card__follow-btn')
+
+followingBtns
+
+followingBtns.forEach(followbtn => followbtn.click())
+
+// 1st do the query step (Find class name and store it in a variable)
+// Perform the process (Click on the button)
