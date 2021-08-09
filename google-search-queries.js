e2e432084c1a513aa62686ca8e3529af2f7f6093
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-08 19:41:53 -0600

    commit on master

---
+++ google-search-queries.js
@@ -0,0 +1,7 @@
+/* 
+DOM = Document Object Model - JS can work with every element on a page
+*/
+
+$('.gLFyf').value = "Javascript tips"; // Search in Google Bar
+
+$(".FPdoLc").childNodes[1].childNodes[1].click() // Search to perform
