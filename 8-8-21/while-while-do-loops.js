cf8e2925ede4eb34dd99bdb0e4a336de7ba15116
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-08 19:20:19 -0600

    commit on master

---
+++ 8-8-21/while-while-do-loops.js
@@ -0,0 +1,17 @@
+var players = [
+    'Altuve',
+    'Bregman',
+    'Correa',
+    'Springer'
+];
+
+var i = 0;
+// while (i < players.length) { // Traditional while loop
+    console.log(players[i]); // If just this runs, infinite loop
+    i++; // Incrementer
+}
+
+// do { // While-do loop
+//     console.log(players[i]);
+//     i++;
+// } while (i < players.length)
