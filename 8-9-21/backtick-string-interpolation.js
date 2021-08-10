f2c9de35f31ccd807bd495a3df3fcc221e176f7b
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-09 20:15:41 -0600

    commit on master

---
+++ 8-9-21/backtick-string-interpolation.js
@@ -0,0 +1,7 @@
+// const lyrics = 'Never gonna give you up'; // Traditional way
+// console.log("I'm " + lyrics);
+
+const lyrics = 'never gonna give you up'; // Modern way
+console.log(`I'm ${lyrics}`);
+console.log(`I'm ${2 + 2}`);
+console.log(`I'm ${lyrics + ' ' + lyrics}`);
