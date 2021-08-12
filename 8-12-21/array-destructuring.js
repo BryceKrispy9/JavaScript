99fc21f3ff5a062e9950474a35bfcc4581ea0038
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-12 17:23:18 -0600

    commit on master

---
+++ 8-12-21/array-destructuring.js
@@ -0,0 +1,14 @@
+const objects = [
+    'Iced Coffee', 
+    'Roses',
+    'Kiwi'
+];
+
+// const object = objects[0]; // Not efficient
+// console.log(object);
+
+const [beverage, plant, fruit] = objects;
+
+console.log(beverage);
+console.log(plant);
+console.log(fruit);
