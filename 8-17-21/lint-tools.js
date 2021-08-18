3ba9422a99a82721bf9b8623674072b6424cf13a
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-17 21:13:04 -0600

    commit on master

---
+++ 8-17-21/lint-tools.js
@@ -0,0 +1,7 @@
+// Linting is the prcess of a library going through your code and giving recommendations - ESLint.org
+
+const greeting = () => {
+    return 'Hello there';
+}
+
+console.log(greeting());
