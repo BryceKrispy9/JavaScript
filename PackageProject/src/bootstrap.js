47cbd4b01573b77e27419485026f3446836e2473
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-21 17:11:06 -0600

    commit on master

--- PackageProject/src/bootstrap.js
+++ PackageProject/src/bootstrap.js
@@ -1,2 +1,31 @@
-// ES6 code goes here
-console.log('es6 code goes here');
+import moment from 'moment';
+
+
+
+// console.log(moment.now()); // Gives ability to get EPOCH timestamp
+
+// const rightNow = moment();
+
+// console.log(rightNow); // Gives us a date, GMT and other features
+
+
+
+// const birthday = moment('1996-04-23', 'YYYY-MM-DD'); 
+
+// function birthday() {
+//     return moment('1996-04-23', 'YYYY-MM-DD');
+// }
+
+// console.log(birthday.fromNow()); // How long ago (not limited in years, but can also do days)
+
+// console.log(birthday.format('dddd, MMMM Do YYYY')); // Date formatter - devhints.io has a cheatsheat for moment.js
+
+
+
+// const twoWeeksFromNow = moment().add(2, 'weeks');
+
+// console.log(twoWeeksFromNow.toString());
+
+const sixMonthsAgo = moment().subtract(6, 'months');
+
+console.log(sixMonthsAgo.toString());
