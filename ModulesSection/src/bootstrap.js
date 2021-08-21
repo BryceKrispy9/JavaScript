bb9f06e916d30cf33342c15500fa76f11c16bd79
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-21 15:33:17 -0600

    commit on master

--- ModulesSection/src/bootstrap.js
+++ ModulesSection/src/bootstrap.js
@@ -1,3 +1,9 @@
-import { multiply } from "./helper";
+import { multiply, greeting } from "./helper";
 
 console.log(multiply(2, 5));
+console.log(greeting);
+
+import * as helper from './helper'; // Multiple from one file
+
+console.log(helper.multiply(5, 6));
+console.log(helper.greeting);
