3d2484d0fab8dad3ab68ae35a4d700a34b1ded14
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-26 21:35:53 -0600

    commit on masterr

---
+++ 8-26-21/window-vs-document.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName('topics');
+
+window.document.getElementsByClassName('topics');
+
+window.alert('Hi there');
+
+// iFrame is where you embed another website inside your own
+
+// document is the HTML file when it communicates with the server
+
+// document lies within the window
