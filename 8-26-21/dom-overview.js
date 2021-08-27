24a4060618914ba5ce7becb3d688150a8ef59877
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-26 20:24:46 -0600

    commit on master

---
+++ 8-26-21/dom-overview.js
@@ -0,0 +1,37 @@
+/* DOM -> Document Object Model
+
+*/
+const user = {
+    name: 'Bryce',
+    email: 'brycepearson09@gmail.com',
+    favorites: [
+        'Chik fil a',
+        "Wendy's"
+    ]
+}
+
+console.log(user.favorites[1]);
+
+/* In HTML browser 
+document.location
+
+document.location.hrep
+
+document.location.protocol
+
+document.body
+
+document.body.parentElement
+
+document.getElementsByClassName('topics');
+
+document.getElementsByClassName('topics')[2];
+
+document.getElementsByClassName('topics')[2].children;
+
+document.getElementsByClassName('topics')[2].children[0];
+
+document.getElementsByClassName('topics')[2].children[0].text;
+*/
+
+// let challenge = document.getElementById('devCamp');
