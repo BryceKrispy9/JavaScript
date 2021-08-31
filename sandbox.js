644580b48b72bb432899b84be10b6e5402220b4f
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-30 19:34:41 -0600

    commit on master

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// Function Declaration
+function greeting() {
+    return 'Hello there';
+}
+
+// Function Expression
+const fullName = function(firstName, lastName) {
+    console.log('Bryce', 'Pearson')
+};
+
+console.log(greeting());
+fullName();
